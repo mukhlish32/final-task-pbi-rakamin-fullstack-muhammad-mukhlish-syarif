aa01ad6115eafd55a5f20bac70e5d643c807df88
mukhlish32@ <mukhlish.syarif@gmail.com>
2024-05-12 23:38:32 +0700

    config: database

---
+++ database/db.go
@@ -0,0 +1,17 @@
+package database
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func ConnectDB() (*gorm.DB, error) {
+	dsn := "host=localhost user=postgres password=sr32 dbname=db_rakamin port=5432 sslmode=disable TimeZone=Asia/Jakarta"
+	db, err := gorm.Open(postgres.New(postgres.Config{
+		DSN: dsn,
+	}), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
